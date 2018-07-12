fcab23312e0077953d5b856a8431b94ce0af0c98
Moises Martinez <moimart@gmail.com>
2018-07-12 10:14:16 +0200

    Updated license file for RLE code

--- lib/ImageRLE.ts
+++ lib/ImageRLE.ts
@@ -1,3 +1,27 @@
+/*
+
+From: https://github.com/kornelski/libicns/blob/master/src/icns_rle24.c
+Copyright (C) 2001-2012 Mathew Eis <mathew@eisbox.net>
+              2007 Thomas Lübking <thomas.luebking@web.de>
+              2002 Chenxiao Zhao <chenxiao.zhao@gmail.com>
+
+Ported to TypeScript by: (C) 2018 Moises Martinez <moises@kikkei.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+
+*/
+
 export class ImageRLE {
   static decodeRLE(data:Buffer, expectedPixelCount:number):Buffer {
 
