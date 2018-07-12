0caa64f085b7c9abb1d1ab8c647a3b238a478a9f
Moises Martinez <moimart@gmail.com>
2018-07-12 10:27:31 +0200

    Fixed declarations

--- IconCreator.d.ts
+++ IconCreator.d.ts
@@ -6,22 +6,22 @@ declare module "IconCreator" {
     public convert(): Promise<string | Buffer>;
   }
 
-  declare abstract class IconCreatorSharp {
+  declare class IconCreatorSharp {
 
     constructor(file: string, output?: string);
     public convert(): Promise<string | Buffer>;
   }
 
-  declare abstract class IconCreatorFile {
+  declare class IconCreatorFile {
 
     constructor(file: string, output?: string);
     public useCLI(cli:boolean);
     public convert(): Promise<string | Buffer>;
   }
 
-  declare abstract class IconCreatorMemory {
+  declare class IconCreatorMemory {
 
     constructor(file: string, output?: string);
     public convert(): Promise<string | Buffer>;
-  }  
+  }
 }
